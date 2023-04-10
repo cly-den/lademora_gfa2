679773ce534c8f376eda79c53a0f080cb13dcaa5
cly-den <125951155+cly-den@users.noreply.github.com>
2023-04-10 23:47:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-alert("Name: Cara \n Height: 5'2\" \n Weight: 125.663 lbs")
-var firstname = prompt("Please enter your first name");
-document.getElementById("fname")
-var lastname = prompt("Please enter your last name");
-document.getElementById("lname")
-var birthyear = prompt("Please enter your Birth Year:");
-document.getElementById("year")
-var age = 2023 - parseInt(birthyear);
-document.getElementById("age")
-
-if(confirm("Is it ok for you to share your personal information with this site? \n Press OK if it's fine \n Cancel if not") == true){
-document.write("Hello " + firstname + " " + lastname + "! How does it feel to be " + age +" years old?")
+function sum(){
+	var num1 = parseInt(document.getElementById("Num1").value);
+	var num2 = parseInt(document.getElementById("Num2").value);
+	result = num1 + num2;
+	document.getElementById("answer").innerHTML = "The sum of " + num1 + " and " + num2 + " is " + result + ".";
 }
-else{
-	alert("User does not wish to share his/her information.");
+function diff(){
+  var num1 = parseInt(document.getElementById("Num1").value);
+  var num2 = parseInt(document.getElementById("Num2").value);
+	result = num1 - num2;
+	document.getElementById("answer").innerHTML = "The difference of " + num1 + " and " + num2 + " is " + result + ".";
+}
+function prod(){
+  var num1 = parseInt(document.getElementById("Num1").value);
+  var num2 = parseInt(document.getElementById("Num2").value);
+	result = num1 * num2;
+	document.getElementById("answer").innerHTML = "The product of " + num1 + " and " + num2 + " is " + result + ".";
+}
+function div(){
+  var num1 = parseInt(document.getElementById("Num1").value);
+  var num2 = parseInt(document.getElementById("Num2").value);
+	result = num1 / num2;
+	document.getElementById("answer").innerHTML = "The quotient of " + num1 + " and " + num2 + " is " + result + ".";
+}
+function mod(){
+  var num1 = parseInt(document.getElementById("Num1").value);
+  var num2 = parseInt(document.getElementById("Num2").value);
+	result = num1 % num2;
+	document.getElementById("answer").innerHTML = "The remainder of " + num1 + " and " + num2 + " is " + result + ".";
 }
